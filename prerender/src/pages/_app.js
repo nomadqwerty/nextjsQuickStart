946c1996ed7f87fd4e7fb35cab9e5141702ea363
nomadqwerty <davexmike72@gmail.com>
2023-04-18 09:04:58 +0100

    header

--- prerender/src/pages/_app.js
+++ prerender/src/pages/_app.js
@@ -1,7 +1,15 @@
 import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <Head>
+        <title>PwaMain</title>
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default MyApp;
